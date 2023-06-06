dca60c0d596d7c733e29bcacd5e08bc3215fbae8
huynhtancuong <huynhtancuong.hcm@gmail.com>
2023-06-06 09:36:16 +0300

    implicit conversions

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,23 +1,10 @@
-struct Retangle {
-    width: u32,
-    height: u32,
-}
-
-impl Retangle {
-    fn new(width: u32, height: u32) -> Retangle {
-        Retangle {width, height,}
-    }
-    fn area(&self) -> u32 {
-        return self.width * self.height;
-    }
-    fn increase_width(&mut self, delta: u32) {
-        self.width += delta;
-    }
+fn multiply(x: i16, y: i16) -> i16 {
+    x * y
 }
 
 fn main() {
-    let mut rect:Retangle = Retangle::new(10, 20);
-    println!("The area of the rectangle is {}", rect.area());
-    rect.increase_width(10);
-    println!("The area of the rectangle is {}", rect.area());
+    let x: i8 = 15;
+    let y: bool = true;
+
+    println!("{x} * {y} = {}", multiply(i16::from(x), y.into()));
 }
