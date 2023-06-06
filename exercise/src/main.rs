bf9ea7e172244aef85fb689f884857757afdc90c
huynhtancuong <huynhtancuong.hcm@gmail.com>
2023-06-06 09:23:35 +0300

    methods

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,24 +1,23 @@
+struct Retangle {
+    width: u32,
+    height: u32,
+}
+
+impl Retangle {
+    fn new(width: u32, height: u32) -> Retangle {
+        Retangle {width, height,}
+    }
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+    fn increase_width(&mut self, delta: u32) {
+        self.width += delta;
+    }
+}
+
 fn main() {
-    print_fizz_buzz_to(100);
-}
-
-fn print_fizz_buzz_to(n: u32) {
-    for i in 1..=n {
-        println!("{}", fizz_buzz(i));
-    }
-}
-
-fn fizz_buzz(n: u32) -> String {
-    let fizz:&str = if is_divisible_by(n, 3) {"Fizz"} else {""};
-    let buzz:&str = if is_divisible_by(n, 5) {"Buzz"} else {""};
-    if fizz.is_empty() && buzz.is_empty() {
-        return format!("{n}")
-    }
-    format!("{fizz}{buzz}")
-}
-/// Determine whether the first argument is divisible by the second argument.
-///
-/// If the second argument is zero, the result is false.
-fn is_divisible_by(n: u32, divisor: u32) -> bool {
-    n % divisor == 0
+    let mut rect:Retangle = Retangle::new(10, 20);
+    println!("The area of the rectangle is {}", rect.area());
+    rect.increase_width(10);
+    println!("The area of the rectangle is {}", rect.area());
 }
